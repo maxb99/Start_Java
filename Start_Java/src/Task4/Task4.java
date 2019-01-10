8dc9195cdc387b54be54a4ffa22fb0fc5fd186b7
maxb99 <33090678+maxb99@users.noreply.github.com>
2019-01-10 21:10:22 +0300

    Update Task4.java

--- Start_Java/src/Task4/Task4.java
+++ Start_Java/src/Task4/Task4.java
@@ -3,8 +3,8 @@ package Task4;
 public class Task4 {
     public static void main(String[] args) {
 
-        int a = 478144;
-        int b = 239071;
+        int a = 234;
+        int b = 10;
 
         if ((a / b) % 10 == 2) {
             System.out.println("True");
