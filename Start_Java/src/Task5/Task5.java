cae250c0bca0479c5e8ff2324b8fa2c2242ee63f
maxb99 <33090678+maxb99@users.noreply.github.com>
2019-01-10 20:52:00 +0300

    Update Task5.java

--- Start_Java/src/Task5/Task5.java
+++ Start_Java/src/Task5/Task5.java
@@ -2,8 +2,8 @@ package Task5;
 
 public class Task5 {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 12;
+        int a = 4;
+        int b = 16;
         int sum = 0;
         for (int i = a; i<=b;i++) {
             sum = sum + i;
