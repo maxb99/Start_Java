8cf3d28fa378b90bcf5fe4c56d6aa5efe4fcd15f
maxb99 <33090678+maxb99@users.noreply.github.com>
2019-01-10 20:48:09 +0300

    Update Task3.java

--- Start_Java/src/Task3/Task3.java
+++ Start_Java/src/Task3/Task3.java
@@ -3,8 +3,8 @@ package Task3;
 import java.util.Arrays;
 
 public class Task3 {
-    public static void main(String[] args) {
-        int[] array = new int[]{11, 321, 3};
+   public static void main(String[] args) {
+        int[] array = new int[]{2, 6, 4};
         Arrays.sort(array);
         System.out.println(Arrays.toString(array));
     }
