9c803300edc2b22275c5c979f9472ea88814f77a
maxb99 <33090678+maxb99@users.noreply.github.com>
2019-01-10 20:54:59 +0300

    Update Task7.java

--- Start_Java/src/Task7/Task7.java
+++ Start_Java/src/Task7/Task7.java
@@ -3,7 +3,7 @@ package Task7;
 public class Task7 {
     public static void main(String[] args) {
         int a = 1;
-        int b = 10;
+        int b = 8;
         for (int i = a, count = 1; i <= b; i++, count++) {
             for (int j = 0; j < count; j++) System.out.print(i);
         }
