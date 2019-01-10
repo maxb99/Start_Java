7659608f9c003fcbcd1f47c8dd34abc8065c4a59
maxb99 <33090678+maxb99@users.noreply.github.com>
2019-01-10 20:47:00 +0300

    Update Task2.java

--- Start_Java/src/Task2/Task2.java
+++ Start_Java/src/Task2/Task2.java
@@ -4,9 +4,9 @@ public class Task2 {
     public static void main(String[] args) {
 
         System.out.println("Даны три числа: 2, 4, 6.");
-        int a = 64;
-        int b = 43;
-        int c = 77;
+        int a = 2;
+        int b = 4;
+        int c = 6;
         int sum = 0;
         if ((a > b) && (b > c)) {
             sum = a + b;
